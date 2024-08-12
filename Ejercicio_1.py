c3e3810d0732fa980a7b525b493aa4bdbbc13871
SACAMO <102098933+GoingNevada@users.noreply.github.com>
2024-08-12 14:57:06 -0500

    Add files via upload

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -2,11 +2,11 @@
 # Autor: Santiago Camargo Molina
 
 def main():
-    lista = input("Ingrese una serie de elementos para la lista, separados por espacio: ").split()
-    print(lista)
-    for i in lista:
-        if lista.count(i)>1:
-            print(f"La lista contiene {lista.count(i)} veces el elemento '{i}'")
+    lista = input("Ingrese una serie de elementos para la lista, separados por espacio: ").split() # Ingreso de datos separados para genera lista
+    print(lista)    # se imprime la lista
+    for i in lista: # iteramos dentro de los elementos de la lista
+        if lista.count(i)>1:    # si el elemento, aparece mas de 1 vez
+            print(f"La lista contiene {lista.count(i)} veces el elemento '{i}'")    # se imprime la cantidad y elemento encontrado
             return None
     print("La lista no contiene elementos repetidos")
     
