2ba886553fcba45cfc5de74204288cf3ca7c6374
SACAMO <102098933+GoingNevada@users.noreply.github.com>
2024-08-12 14:57:38 -0500

    Add files via upload

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -2,15 +2,15 @@
 # Autor: Santiago Camargo Molina
 
 def main():
-    lista = input("Ingrese una serie de cadenas de caracteres separadas por espacio: ").split()
-    vocales = ['a','e','i','o','u']
-    print(lista)
-    for i in lista:
-        count = 0
-        for j in vocales:
-            if j in i:
+    lista = input("Ingrese una serie de cadenas de caracteres separadas por espacio: ").split() # ingreso de las cadenas de caracteres
+    vocales = ['a','e','i','o','u'] # lista que contiene las vocales
+    print(lista)    # se imprime la lista
+    for i in lista: # iteramos dentro de la lista
+        count = 0   # contador de veces que se ha encontrado una vocal
+        for j in vocales:   # iterador para vocales
+            if j in i:  # si la vocal esta en la cadena, se suma 1 al contador
                 count +=1
-        if count > 1:    
+        if count > 1:    # si el contador termina siendo mayor a 1, entonces se envia mensaje
             print(f"La cadena '{i}' contiene mas de una vocal")
             return None
     print("No existe ninguna cadena dentro de la lista, con mas de una vocal")
--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -2,13 +2,13 @@
 # Autor: Santiago Camargo Molina
 
 def main():
-    lista1 = input("Ingrese una serie de elementos separados con espacio: ").split()
-    lista2 = input("Ingrese una serie de elementos separados con espacio: ").split()
-    n_list = []
-    for i in lista1:
-        if i not in lista2:
+    lista1 = input("Ingrese una serie de elementos separados con espacio: ").split() # ingreso de cadena 1
+    lista2 = input("Ingrese una serie de elementos separados con espacio: ").split() # ingreso de cadena 2
+    n_list = [] # lista donde se almacenaran los elementos de 1 que no estan en 2
+    for i in lista1:    # iteramos dentro de la lista 1
+        if i not in lista2: # si el elemento no esta en la lista dos, entonces se agrega a la lista vacia
             n_list.append(i)
-    if len(n_list)>0:
+    if len(n_list)>0:   # si el tamaño final de la lista es >0, entonces se imprimen los elementos encontrados
         print(f"La lista dos, no contiene los elementos: {n_list}, de la lista uno")
     else:
         print("Las dos listas, contienen los mismo elementos")
